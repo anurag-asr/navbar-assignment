bc063bf8db74aa8f111f561a3ac4c88cb384cb9f
anurag-asr <anurjwt@gmail.com>
2022-05-19 11:15:27 +0530

    first commit

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h2>LOCOBAKERY</h2>
+      <div className='navbar1'>
+          <p>Service</p>
+          <p>Projects</p>
+          <p>About</p>
+          <button>Contact</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
